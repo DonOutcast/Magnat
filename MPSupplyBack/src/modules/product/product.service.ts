34ff601fc3410e296edb7d0c7c5f6b23d6b4e9e3
DonOutcast <sham1996@yandex.ru>
2025-06-07 17:55:38 +0300

    added logs

--- MPSupplyBack/src/modules/product/product.service.ts
+++ MPSupplyBack/src/modules/product/product.service.ts
@@ -1,237 +1,258 @@
-import { Injectable } from '@nestjs/common';
-import { AutoCompleteDto } from 'src/dto/autocomplete.dto';
-import { Op } from 'sequelize';
-import { InjectModel } from '@nestjs/sequelize';
-import { Product, ProductF } from './entities/product.entity';
-import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
-import { SettingsService } from '../settings/settings.service';
-import { API_GO_HOSTS, MP, ProductAttributesResponseOzon, ProductsResponseWB, SETTINGS, SETTINGS_MODULE } from 'src/main.types';
-import { isEmpty } from 'class-validator';
+import {Injectable} from '@nestjs/common';
+import {AutoCompleteDto} from 'src/dto/autocomplete.dto';
+import {Op} from 'sequelize';
+import {InjectModel} from '@nestjs/sequelize';
+import {Product, ProductF} from './entities/product.entity';
+import {HttpService} from '@nestjs/axios';
+import {firstValueFrom} from 'rxjs';
+import {SettingsService} from '../settings/settings.service';
+import {
+    API_GO_HOSTS,
+    MP,
+    ProductAttributesResponseOzon,
+    ProductsResponseWB,
+    SETTINGS,
+    SETTINGS_MODULE
+} from 'src/main.types';
+import {isEmpty} from 'class-validator';
+import {Logger} from '@nestjs/common';
 
 @Injectable()
 export class ProductService {
-  constructor(
-    @InjectModel(Product) private productRepository: typeof Product,
-    private readonly httpService: HttpService,
-    private readonly settingsService: SettingsService,
-  ) {}
-  async autocomplete(dto: AutoCompleteDto, cid: number) {
-    const where = {
-      [Op.or]: {
-        name: { [Op.iLike]: '%' + dto.search + '%' },
-        offer_id: { [Op.iLike]: '%' + dto.search + '%' },
-      },
-      cid,
-    };
+    private readonly logger = new Logger(ProductService.name);
 
-    const res = await this.productRepository.findAll({
-      where: where,
-      limit: dto.limit,
-    });
-
-    return res;
-  }
-
-  async findAllByMP(cid: number, mp: string) {
-    return await this.productRepository.findAll({
-      where: { cid, mp: mp },
-    });
-  }
-
-  getVolume(item: ProductAttributesResponseOzon): number {
-    const cubic = item.height * item.depth * item.width;
-    switch (item.dimension_unit) {
-      case 'mm':
-        return cubic / 1000000;
-      case 'cm':
-        return cubic / 1000;
-      case 'in':
-        return cubic / 61.024;
-      default:
-        return cubic;
-    }
-  }
-
-  async sync(cid: number) {
-    return {
-      [MP.Ozon]: await this.syncOzon(cid),
-      [MP.WB]: await this.syncWB(cid),
-    };
-  }
-
-  async syncOzon(cid: number) {
-    const productIds: number[] = await this.getProductListOzon(cid);
-
-    if (productIds === null || productIds.length === 0) {
-      return { total: 0, new: 0 };
+    constructor(
+        @InjectModel(Product) private productRepository: typeof Product,
+        private readonly httpService: HttpService,
+        private readonly settingsService: SettingsService,
+    ) {
     }
 
-    const existIds = (
-      await this.productRepository.findAll({
-        attributes: [ProductF.FOREIGN_ID],
-        where: { cid, mp: MP.Ozon },
-      })
-    ).map((el) => +el.foreignId);
-
-    const newIds = productIds.filter((el) => existIds.indexOf(el) === -1);
-    const oldIds = productIds.filter((el) => existIds.indexOf(el) !== -1);
-
-    if (newIds.length > 0) {
-      const newProducts = await this.getProductListInfoOzon(newIds, cid);
-      if (newProducts) {
-        await this.productRepository.bulkCreate(
-          newProducts.map((el: any) => ({
-            ...el,
-            cid,
-            sku: el.sources[0].sku,
-            barcode: el.barcodes[0],
-            foreignId: el.id,
-            id: null,
-            mp: MP.Ozon,
-          })),
-        );
-      }
-    }
-
-    if (oldIds.length > 0) {
-      const oldProducts = await this.getProductListInfoOzon(oldIds, cid);
-
-      if (oldProducts) {
-        oldProducts.forEach((el) => {
-          this.productRepository.update(
-            {
-              name: el.name,
-              offer_id: el.offer_id,
-              barcode: el.barcodes[0],
-              sku: el.sources[0].sku,
+    async autocomplete(dto: AutoCompleteDto, cid: number) {
+        const where = {
+            [Op.or]: {
+                name: {[Op.iLike]: '%' + dto.search + '%'},
+                offer_id: {[Op.iLike]: '%' + dto.search + '%'},
             },
-            { where: { foreignId: el.id, cid, mp: MP.Ozon } },
-          );
+            cid,
+        };
+
+        const res = await this.productRepository.findAll({
+            where: where,
+            limit: dto.limit,
         });
-      }
+
+        return res;
     }
 
-    const products = await this.productRepository.findAll({
-      attributes: [ProductF.FOREIGN_ID],
-      where: { volume: null, cid, mp: MP.Ozon },
-    });
-
-    if (products) {
-      const productAttributes = await this.getProductListAttributesOzon(
-        products.map((el) => +el.foreignId),
-        cid,
-      );
-
-      if (productAttributes) {
-        productAttributes.forEach((el) => {
-          this.productRepository.update({ volume: this.getVolume(el) }, { where: { foreignId: el.id, cid, mp: MP.Ozon } });
+    async findAllByMP(cid: number, mp: string) {
+        return await this.productRepository.findAll({
+            where: {cid, mp: mp},
         });
-      }
     }
 
-    return { total: productIds.length, new: products.length };
-  }
+    getVolume(item: ProductAttributesResponseOzon): number {
+        const cubic = item.height * item.depth * item.width;
+        switch (item.dimension_unit) {
+            case 'mm':
+                return cubic / 1000000;
+            case 'cm':
+                return cubic / 1000;
+            case 'in':
+                return cubic / 61.024;
+            default:
+                return cubic;
+        }
+    }
 
-  async getProductListOzon(cid: number): Promise<number[]> {
-    let { data } = await firstValueFrom(
-      this.httpService.get(API_GO_HOSTS.SELLER_OZON + '/products', {
-        headers: {
-          'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
-          'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
-        },
-      }),
-    );
+    async sync(cid: number) {
+        this.logger.log(`Начало синхронизации для кабинета cid=${cid}`);
+        const result = {
+            [MP.Ozon]: await this.syncOzon(cid),
+            [MP.WB]: await this.syncWB(cid),
+        };
+        this.logger.log(`Завершена синхронизация для кабинета cid=${cid}`, JSON.stringify(result));
+        return result;
+    }
 
-    return data;
-  }
+    async syncOzon(cid: number) {
+        const productIds: number[] = await this.getProductListOzon(cid);
 
-  async getProductListInfoOzon(ids: number[], cid: number) {
-    let { data } = await firstValueFrom(
-      this.httpService.post(
-        API_GO_HOSTS.SELLER_OZON + '/products/info',
-        {
-          product_id: ids,
-        },
-        {
-          headers: {
-            'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
-            'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
-          },
-        },
-      ),
-    );
+        if (productIds === null || productIds.length === 0) {
+            return {total: 0, new: 0};
+        }
 
-    return data;
-  }
+        const existIds = (
+            await this.productRepository.findAll({
+                attributes: [ProductF.FOREIGN_ID],
+                where: {cid, mp: MP.Ozon},
+            })
+        ).map((el) => +el.foreignId);
 
-  async getProductListAttributesOzon(ids: number[], cid: number): Promise<ProductAttributesResponseOzon[]> {
-    let { data } = await firstValueFrom(
-      this.httpService.post(
-        API_GO_HOSTS.SELLER_OZON + '/products/attributes',
-        {
-          product_id: ids,
-        },
-        {
-          headers: {
-            'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
-            'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
-          },
-        },
-      ),
-    );
+        const newIds = productIds.filter((el) => existIds.indexOf(el) === -1);
+        const oldIds = productIds.filter((el) => existIds.indexOf(el) !== -1);
 
-    return data;
-  }
+        if (newIds.length > 0) {
+            const newProducts = await this.getProductListInfoOzon(newIds, cid);
+            if (newProducts) {
+                await this.productRepository.bulkCreate(
+                    newProducts.map((el: any) => ({
+                        ...el,
+                        cid,
+                        sku: el.sources[0].sku,
+                        barcode: el.barcodes[0],
+                        foreignId: el.id,
+                        id: null,
+                        mp: MP.Ozon,
+                    })),
+                );
+            }
+        }
 
-  async toArchive(id: number, cid: number) {
-    await this.productRepository.update({ inArchive: true }, { where: { id: id, cid } });
-  }
+        if (oldIds.length > 0) {
+            const oldProducts = await this.getProductListInfoOzon(oldIds, cid);
 
-  async fromArchive(id: number, cid: number) {
-    await this.productRepository.update({ inArchive: false }, { where: { id: id, cid } });
-  }
+            if (oldProducts) {
+                oldProducts.forEach((el) => {
+                    this.productRepository.update(
+                        {
+                            name: el.name,
+                            offer_id: el.offer_id,
+                            barcode: el.barcodes[0],
+                            sku: el.sources[0].sku,
+                        },
+                        {where: {foreignId: el.id, cid, mp: MP.Ozon}},
+                    );
+                });
+            }
+        }
 
-  async notRequired(cid: number) {
-    return await this.productRepository.findAll({
-      where: { cid, inArchive: true },
-    });
-  }
+        const products = await this.productRepository.findAll({
+            attributes: [ProductF.FOREIGN_ID],
+            where: {volume: null, cid, mp: MP.Ozon},
+        });
 
-  async syncWB(cid: number) {
-    const existsBySku = (await this.findAllByMP(cid, MP.WB)).reduce((r, el) => {
-      r[el.sku] = el;
-      return r;
-    }, {});
+        if (products) {
+            const productAttributes = await this.getProductListAttributesOzon(
+                products.map((el) => +el.foreignId),
+                cid,
+            );
 
-    const products = await this.getProductsWB(cid);
+            if (productAttributes) {
+                productAttributes.forEach((el) => {
+                    this.productRepository.update({volume: this.getVolume(el)}, {
+                        where: {
+                            foreignId: el.id,
+                            cid,
+                            mp: MP.Ozon
+                        }
+                    });
+                });
+            }
+        }
 
-    await this.productRepository.bulkCreate(
-      products
-        .filter((el) => !(el.sku in existsBySku))
-        .map((el) => ({
-          ...el,
-          cid,
-          mp: MP.WB,
-        })),
-    );
+        return {total: productIds.length, new: products.length};
+    }
 
-    return { total: products.length, new: products.filter((el) => !(el.sku in existsBySku)).length };
-  }
+    async getProductListOzon(cid: number): Promise<number[]> {
+        let {data} = await firstValueFrom(
+            this.httpService.get(API_GO_HOSTS.SELLER_OZON + '/products', {
+                headers: {
+                    'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
+                    'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
+                },
+            }),
+        );
 
-  async getProductsWB(cid: number): Promise<ProductsResponseWB[]> {
-    const apiKey = await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.WB_API_KEY, cid);
+        return data;
+    }
 
-    if (isEmpty(apiKey)) return [];
+    async getProductListInfoOzon(ids: number[], cid: number) {
+        let {data} = await firstValueFrom(
+            this.httpService.post(
+                API_GO_HOSTS.SELLER_OZON + '/products/info',
+                {
+                    product_id: ids,
+                },
+                {
+                    headers: {
+                        'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
+                        'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
+                    },
+                },
+            ),
+        );
 
-    let { data } = await firstValueFrom(
-      this.httpService.get(API_GO_HOSTS.SELLER_WB + '/products', {
-        headers: {
-          Authorization: await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.WB_API_KEY, cid),
-        },
-      }),
-    );
+        return data;
+    }
 
-    return data ?? [];
-  }
+    async getProductListAttributesOzon(ids: number[], cid: number): Promise<ProductAttributesResponseOzon[]> {
+        let {data} = await firstValueFrom(
+            this.httpService.post(
+                API_GO_HOSTS.SELLER_OZON + '/products/attributes',
+                {
+                    product_id: ids,
+                },
+                {
+                    headers: {
+                        'Client-Id': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_CLIENT_ID, cid),
+                        'Api-Key': await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.OZON_API_KEY, cid),
+                    },
+                },
+            ),
+        );
+
+        return data;
+    }
+
+    async toArchive(id: number, cid: number) {
+        await this.productRepository.update({inArchive: true}, {where: {id: id, cid}});
+    }
+
+    async fromArchive(id: number, cid: number) {
+        await this.productRepository.update({inArchive: false}, {where: {id: id, cid}});
+    }
+
+    async notRequired(cid: number) {
+        return await this.productRepository.findAll({
+            where: {cid, inArchive: true},
+        });
+    }
+
+    async syncWB(cid: number) {
+        const existsBySku = (await this.findAllByMP(cid, MP.WB)).reduce((r, el) => {
+            r[el.sku] = el;
+            return r;
+        }, {});
+
+        const products = await this.getProductsWB(cid);
+
+        await this.productRepository.bulkCreate(
+            products
+                .filter((el) => !(el.sku in existsBySku))
+                .map((el) => ({
+                    ...el,
+                    cid,
+                    mp: MP.WB,
+                })),
+        );
+
+        return {total: products.length, new: products.filter((el) => !(el.sku in existsBySku)).length};
+    }
+
+    async getProductsWB(cid: number): Promise<ProductsResponseWB[]> {
+        const apiKey = await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.WB_API_KEY, cid);
+
+        if (isEmpty(apiKey)) return [];
+
+        let {data} = await firstValueFrom(
+            this.httpService.get(API_GO_HOSTS.SELLER_WB + '/products', {
+                headers: {
+                    Authorization: await this.settingsService.get(SETTINGS_MODULE.API, SETTINGS.WB_API_KEY, cid),
+                },
+            }),
+        );
+
+        return data ?? [];
+    }
 }
