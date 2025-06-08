1e7603dbea0a1575bbbbab1258cfe4f629978634
DonOutcast <sham1996@yandex.ru>
2025-06-08 13:50:00 +0300

    added logs

--- GOlangAPIs/OzonRequest/api_result.go
+++ GOlangAPIs/OzonRequest/api_result.go
@@ -17,42 +17,53 @@ type ApiResult[T any] struct {
 	Data      T
 	exception error
 }
-
 func PostRequest[T any, V any](data T, link string, userReq http.Request) ApiResult[V] {
 	var result ApiResult[V]
 
 	jsonData, err := json.Marshal(data)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	if reflect.TypeOf(data) == nil {
-		jsonData = nil
+		log.Printf("Ошибка при сериализации: %v", err)
+		result.IsOk = false
+		return result
 	}
 
 	client := http.Client{}
 
 	req, err := http.NewRequest("POST", "https://api-seller.ozon.ru"+link, bytes.NewReader(jsonData))
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Ошибка при создании запроса: %v", err)
+		result.IsOk = false
+		return result
 	}
 
-	req.Header.Add("ContentType", "application/json")
-	req.Header.Add("Client-Id", userReq.Header.Get("Client-Id"))
-	req.Header.Add("Api-Key", userReq.Header.Get("Api-Key"))
-	resp, err := client.Do(req)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Client-Id", userReq.Header.Get("Client-Id"))
+	req.Header.Set("Api-Key", userReq.Header.Get("Api-Key"))
 
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Ошибка при выполнении запроса: %v", err)
+		result.IsOk = false
+		return result
 	}
 	defer resp.Body.Close()
 
-	json.NewDecoder(resp.Body).Decode(&result.Data)
-	result.IsOk = true
-	result.exception = nil
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Ответ от сервера: %s", resp.Status)
+		result.IsOk = false
+		return result
+	}
 
+	if err := json.NewDecoder(resp.Body).Decode(&result.Data); err != nil {
+		log.Printf("Ошибка при разборе JSON-ответа: %v", err)
+		result.IsOk = false
+		return result
+	}
+
+	result.IsOk = true
 	return result
 }
+
 
 func GetRequest[T any, V any](link string, userReq http.Request) ApiResult[V] {
 	var result ApiResult[V]
@@ -64,7 +75,7 @@ func GetRequest[T any, V any](link string, userReq http.Request) ApiResult[V] {
 		log.Fatal(err)
 	}
 
-	req.Header.Add("ContentType", "application/json")
+	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Client-Id", userReq.Header.Get("Client-Id"))
 	req.Header.Add("Api-Key", userReq.Header.Get("Api-Key"))
 	resp, err := client.Do(req)
